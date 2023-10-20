f3aadd562d9c335d6066e4820875c8ca27d4cbbd
Riccardo Perra <perrariccardo0@gmail.com>
2023-10-20 19:09:12 +0200

    supabase env

--- src/core/supabase.ts
+++ src/core/supabase.ts
@@ -4,9 +4,11 @@ import { cookieStorage } from "./utils/cookieStorage";
 
 export const supabaseCookieName = "sb-token";
 
+const { CLIENT_SUPABASE_URL, CLIENT_SUPABASE_KEY } = import.meta.env;
+
 export const supabase = createClient<Database>(
-  "https://byxdpolwzavnqnqtyqbs.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ5eGRwb2x3emF2bnFucXR5cWJzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTEyMzM1ODAsImV4cCI6MjAwNjgwOTU4MH0.H0FWdmwAC5nkLakUDihMWKzgYBN_paeWMz8pfpisBd8",
+  CLIENT_SUPABASE_URL,
+  CLIENT_SUPABASE_KEY,
   {
     auth: {
       storage: undefined,
