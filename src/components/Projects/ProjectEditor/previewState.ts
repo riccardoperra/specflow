d2baa46405cfec7e5607d7cade5537091979a0a2
Riccardo Perra <perrariccardo0@gmail.com>
2023-10-21 17:43:16 +0200

    improve export

--- src/components/Projects/ProjectEditor/previewState.ts
+++ src/components/Projects/ProjectEditor/previewState.ts
@@ -1,6 +1,7 @@
 import { defineSignal } from "statebuilder";
 import { withProxyCommands } from "statebuilder/commands";
 import { withAsyncAction } from "statebuilder/asyncAction";
+import { createUniqueId } from "solid-js";
 
 type Commands = {
   setRef: HTMLElement;
@@ -13,21 +14,45 @@ export const PreviewState = defineSignal<HTMLElement | null>(() => null)
     _.hold(_.commands.setRef, (ref) => _.set(() => ref));
   })
   .extend((_) => {
+    const exportPreview = (
+      svg: SVGElement,
+      exportFn: (node: HTMLElement) => Promise<any>,
+    ) => {
+      const uuid = createUniqueId();
+      const id = `export-preview-${uuid}`;
+      const preview = document.createElement("div");
+      preview.setAttribute("id", id);
+      // TODO: should be customizable
+      preview.classList.add("bg-neutral-800");
+      preview.style.position = "absolute";
+      preview.style.left = "0px";
+      preview.style.top = "0px";
+      preview.style.zIndex = "-1";
+      preview.style.width = "fit-content";
+      preview.style.height = "fit-content";
+      const clonedSvg = svg.cloneNode(true) as SVGElement;
+      clonedSvg.style.removeProperty("max-width");
+      clonedSvg.style.removeProperty("transform-origin");
+      clonedSvg.style.removeProperty("transform");
+      preview.appendChild(clonedSvg);
+      document.body.appendChild(preview);
+      return exportFn(preview).finally(() => preview.remove());
+    };
+
     const node = () => {
       const ref = _()!;
-      const svg = ref.firstChild! as SVGElement;
-      svg.style.removeProperty("max-width");
-      svg.style.removeProperty("transform-origin");
-      svg.style.removeProperty("transform");
-      return ref;
+      return ref.firstChild as SVGElement;
     };
 
     const domToBlob = () =>
       import("modern-screenshot").then((m) =>
-        m.domToBlob(node(), {
-          scale: 3,
-          style: { padding: "0px", margin: "0px" },
-        }),
+        exportPreview(node(), (svg) =>
+          m.domToBlob(svg, {
+            scale: 6,
+            debug: true,
+            style: { padding: "0px", margin: "0px" },
+          }),
+        ),
       );
 
     const openToExternalWindow = _.asyncAction(() =>
